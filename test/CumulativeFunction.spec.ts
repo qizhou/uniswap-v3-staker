96944a7e8619a4dda1992a610f5b7d0782096e17
Lint Action <lint-action@samuelmeuli.com>
2021-06-21 18:40:09 +0000

    Fix code style issues with Prettier

--- test/CumulativeFunction.spec.ts
+++ test/CumulativeFunction.spec.ts
@@ -45,58 +45,58 @@ describe('CumulativeFunction', () => {
 
   describe('#add/remove', () => {
     it('add/remove single', async () => {
-      await cfTest.add(5342, 10);
-      await cfTest.remove(5342, 10);
-      expect(await cfTest.get(153)).to.eq('0');
-      expect(await cfTest.get(5341)).to.eq('0');
-      expect(await cfTest.get(5342)).to.eq('0');
-      expect(await cfTest.get(15342)).to.eq('0');
+      await cfTest.add(5342, 10)
+      await cfTest.remove(5342, 10)
+      expect(await cfTest.get(153)).to.eq('0')
+      expect(await cfTest.get(5341)).to.eq('0')
+      expect(await cfTest.get(5342)).to.eq('0')
+      expect(await cfTest.get(15342)).to.eq('0')
     })
 
     it('add/remove two', async () => {
-      await cfTest.add(1234, 10);
-      await cfTest.add(5678, 10);
-      await cfTest.remove(1234, 10);
-      expect(await cfTest.get(153)).to.eq('0');
-      expect(await cfTest.get(1233)).to.eq('0');
-      expect(await cfTest.get(1234)).to.eq('0');
-      expect(await cfTest.get(5677)).to.eq('0');
-      expect(await cfTest.get(5678)).to.eq('10');
-      expect(await cfTest.get(56780)).to.eq('10');
-      await cfTest.remove(5678, 10);
-      expect(await cfTest.get(153)).to.eq('0');
-      expect(await cfTest.get(1233)).to.eq('0');
-      expect(await cfTest.get(1234)).to.eq('0');
-      expect(await cfTest.get(5677)).to.eq('0');
-      expect(await cfTest.get(5678)).to.eq('0');
-      expect(await cfTest.get(56780)).to.eq('0');
+      await cfTest.add(1234, 10)
+      await cfTest.add(5678, 10)
+      await cfTest.remove(1234, 10)
+      expect(await cfTest.get(153)).to.eq('0')
+      expect(await cfTest.get(1233)).to.eq('0')
+      expect(await cfTest.get(1234)).to.eq('0')
+      expect(await cfTest.get(5677)).to.eq('0')
+      expect(await cfTest.get(5678)).to.eq('10')
+      expect(await cfTest.get(56780)).to.eq('10')
+      await cfTest.remove(5678, 10)
+      expect(await cfTest.get(153)).to.eq('0')
+      expect(await cfTest.get(1233)).to.eq('0')
+      expect(await cfTest.get(1234)).to.eq('0')
+      expect(await cfTest.get(5677)).to.eq('0')
+      expect(await cfTest.get(5678)).to.eq('0')
+      expect(await cfTest.get(56780)).to.eq('0')
     })
 
     it('add/remove three', async () => {
-      await cfTest.add(1234, 10);
-      await cfTest.add(5678, 10);
-      await cfTest.add(8678, 10);
-      await cfTest.remove(8678, 10);
-      expect(await cfTest.get(153)).to.eq('0');
-      expect(await cfTest.get(1233)).to.eq('0');
-      expect(await cfTest.get(1234)).to.eq('10');
-      expect(await cfTest.get(5677)).to.eq('10');
-      expect(await cfTest.get(5678)).to.eq('20');
-      expect(await cfTest.get(56780)).to.eq('20');
-      await cfTest.remove(5678, 10);
-      expect(await cfTest.get(153)).to.eq('0');
-      expect(await cfTest.get(1233)).to.eq('0');
-      expect(await cfTest.get(1234)).to.eq('10');
-      expect(await cfTest.get(5677)).to.eq('10');
-      expect(await cfTest.get(5678)).to.eq('10');
-      expect(await cfTest.get(56780)).to.eq('10');
-      await cfTest.remove(1234, 10);
-      expect(await cfTest.get(153)).to.eq('0');
-      expect(await cfTest.get(1233)).to.eq('0');
-      expect(await cfTest.get(1234)).to.eq('0');
-      expect(await cfTest.get(5677)).to.eq('0');
-      expect(await cfTest.get(5678)).to.eq('0');
-      expect(await cfTest.get(56780)).to.eq('0');
+      await cfTest.add(1234, 10)
+      await cfTest.add(5678, 10)
+      await cfTest.add(8678, 10)
+      await cfTest.remove(8678, 10)
+      expect(await cfTest.get(153)).to.eq('0')
+      expect(await cfTest.get(1233)).to.eq('0')
+      expect(await cfTest.get(1234)).to.eq('10')
+      expect(await cfTest.get(5677)).to.eq('10')
+      expect(await cfTest.get(5678)).to.eq('20')
+      expect(await cfTest.get(56780)).to.eq('20')
+      await cfTest.remove(5678, 10)
+      expect(await cfTest.get(153)).to.eq('0')
+      expect(await cfTest.get(1233)).to.eq('0')
+      expect(await cfTest.get(1234)).to.eq('10')
+      expect(await cfTest.get(5677)).to.eq('10')
+      expect(await cfTest.get(5678)).to.eq('10')
+      expect(await cfTest.get(56780)).to.eq('10')
+      await cfTest.remove(1234, 10)
+      expect(await cfTest.get(153)).to.eq('0')
+      expect(await cfTest.get(1233)).to.eq('0')
+      expect(await cfTest.get(1234)).to.eq('0')
+      expect(await cfTest.get(5677)).to.eq('0')
+      expect(await cfTest.get(5678)).to.eq('0')
+      expect(await cfTest.get(56780)).to.eq('0')
     })
   })
 })
