f16fd46309383230a41e52562c16b865339c6716
ninjaahhh <38544039+ninjaahhh@users.noreply.github.com>
2021-06-22 15:50:16 -0700

    Update base tests for universal staker (#9)
    * add rewardCalc, change IncentiveId func
    
    * fix deposit except gas cost
    
    * fix incentive test
    
    * fix multicall test
    
    * fix stake test
    
    * Bring back gas snapshots
    
    * minor
    
    * prettier
    
    Co-authored-by: pichao <pichao@quarkchain.org>

--- contracts/test/TestIncentiveId.sol
+++ contracts/test/TestIncentiveId.sol
@@ -2,13 +2,12 @@
 pragma solidity =0.7.6;
 pragma abicoder v2;
 
-import '../interfaces/IUniswapV3Staker.sol';
-
-import '../libraries/IncentiveId.sol';
+import '../interfaces/IUniversalV3Staker.sol';
+import '../libraries/UniversalIncentiveId.sol';
 
 /// @dev Test contract for IncentiveId
 contract TestIncentiveId {
-    function compute(IUniswapV3Staker.IncentiveKey memory key) public pure returns (bytes32) {
-        return IncentiveId.compute(key);
+    function compute(IUniversalV3Staker.IncentiveKey memory key) public pure returns (bytes32) {
+        return UniversalIncentiveId.compute(key);
     }
 }
